2f5009beef1744a1a9dd495fe4a46d6f5758d4d7
Rifai <84585203+rifainareswara@users.noreply.github.com>
2025-04-03 16:57:02 +0700

    Add environment-specific asset loading for production and local environments

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,13 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @if(app()->environment('local'))
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+@else
+    <link rel="stylesheet" href="{{ asset('build/assets/app-iIK3QGs2.css') }}">
+    <script src="{{ asset('build/assets/app-BPnfBaih.js') }}" defer></script>
+@endif
+
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
